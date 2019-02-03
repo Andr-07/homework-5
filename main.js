4e275e857032f9d42558b007b6623591797e23f7
Andr-07 <46841332+Andr-07@users.noreply.github.com>
2019-02-03 18:16:07 +0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,28 @@
+var square = document.querySelector('.dragElement');
+
+square.onmousedown = function(e) {
+  square.style.position = 'absolute';
+  moveAt(e);
+
+  document.body.appendChild(square);
+
+  square.style.zIndex = 1000;
+
+
+  function moveAt(e) {
+    square.style.left = e.pageX - square.offsetWidth / 2 + 'px';
+    square.style.top = e.pageY - square.offsetHeight / 2 + 'px';
+  }
+
+  document.onmousemove = function(e) {
+    moveAt(e);
+  }
+
+  square.onmouseup = function() {
+    document.onmousemove = null;
+    square.onmouseup = null;
+  }
+  square.ondragstart = function() {
+  return false;
+};
+}
